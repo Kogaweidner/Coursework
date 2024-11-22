9cbfec8e97eac5e1c8d81c8e51df690cb4c2be3b
DaffaHavez <daffa.havez00@gmail.com>
2024-11-22 15:35:19 +0400

    Products list

---
+++ products.js
@@ -0,0 +1,92 @@
+let products = [
+    {
+        "id":1001,
+        "title":"Math Class ",
+        "location": "London ",
+        "price":100,
+        "image": "images/math.png",
+        "availableInventory":6,
+        "rating":2
+    },
+    {
+        "id":1002,
+        "title":"Art class ",
+        "location": "Yorkshire ",
+        "price":150,
+        "image": "images/math.png",
+        "availableInventory":6,
+        "rating":2
+    },
+    {
+        "id":1003,
+        "title":"IT class ",
+        "location": "London ",
+        "price":200,
+        "image": "images/math.png",
+        "availableInventory":6,
+        "rating":2
+    },
+    {
+        "id":1004,
+        "title":"IT Class ",
+        "location": "Manchester ",
+        "price":200,
+        "image": "images/math.png",
+        "availableInventory":6,
+        "rating":2
+    },
+    {
+        "id":1005,
+        "title":"Geography Class ",
+        "location": "Manchester ",
+        "price":200,
+        "image": "images/math.png",
+        "availableInventory":6,
+        "rating":2
+    },
+    {
+        "id":1006,
+        "title":"Geography Class ",
+        "location": "Leeds ",
+        "price":200,
+        "image": "images/math.png",
+        "availableInventory":6,
+        "rating":2
+    },
+    {
+        "id":1007,
+        "title":"Art Class ",
+        "location": "Liverpool ",
+        "price":200,
+        "image": "images/math.png",
+        "availableInventory":6,
+        "rating":2
+    },
+    {
+        "id":1008,
+        "title":"Chemistry Class ",
+        "location": "London ",
+        "price":200,
+        "image": "images/math.png",
+        "availableInventory":6,
+        "rating":2
+    },
+    {
+        "id":1009,
+        "title":"Chemistry Class ",
+        "location": "Birmingham ",
+        "price":200,
+        "image": "images/math.png",
+        "availableInventory":6,
+        "rating":2
+    },
+    {
+        "id":1010,
+        "title":"Math Class ",
+        "location": "Cambridge ",
+        "price":200,
+        "image": "images/math.png",
+        "availableInventory":6,
+        "rating":2
+    },
+]   
